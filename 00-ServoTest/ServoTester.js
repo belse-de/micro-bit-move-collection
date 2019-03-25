cf4547b6d133fd26efbc6e7bc102b8c4a76718bd
belse-de <belse.de@gmail.com>
2019-03-25 01:49:40 +0100

    added a servo test

---
+++ 00-ServoTest/ServoTester.js
@@ -0,0 +1,16 @@
+input.onButtonPressed(Button.A, function () {
+    pins.servoWritePin(AnalogPin.P1, 180)
+    pins.servoWritePin(AnalogPin.P2, 180)
+})
+input.onButtonPressed(Button.B, function () {
+    pins.servoWritePin(AnalogPin.P1, 0)
+    pins.servoWritePin(AnalogPin.P2, 0)
+})
+input.onButtonPressed(Button.AB, function () {
+    pins.servoWritePin(AnalogPin.P1, 90)
+    pins.servoWritePin(AnalogPin.P2, 90)
+})
+basic.forever(function () {
+	
+})
+
